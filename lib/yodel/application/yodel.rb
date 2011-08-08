23c17ea828a340da8105293e4ea5cbde2cb745f7
Will Cannings <me@willcannings.com>
2011-08-08 21:25:06 +1000

    Fix issue where extension loading was silently failing and no extensions were being loaded

--- lib/yodel/application/yodel.rb
+++ lib/yodel/application/yodel.rb
@@ -10,7 +10,10 @@ module Yodel
   def self.load_extensions
     extensions = Yodel.config.root.join('extensions')
     return unless extensions.exist?
-    extensions.each_entry {|extension| load_extension(extension)}
+    extensions.each_entry do |extension|
+      next if extension.to_s.start_with?('.')
+      load_extension(extension.realpath(extensions))
+    end
   end
   
   def self.load_extension(path)
@@ -24,7 +27,15 @@ module Yodel
     Yodel.config.public_directories << public_dir if File.directory?(public_dir)
     Yodel.config.layout_directories << layouts_dir if File.directory?(layouts_dir)
     Yodel.config.migration_directories.insert(-2, migration_dir) if File.directory?(migration_dir)
-    models_dir.each_entry {|model| require model} if models_dir.exist?
+    
+    # load any models. if the init.rb file exists it will have been loaded first,
+    # allowing extensions to specify the order models are loaded.
+    if models_dir.exist?
+      models_dir.each_entry do |model|
+        next if model.to_s.start_with?('.')
+        require model.realpath(models_dir)
+      end
+    end
   end
   
   def self.use_middleware(&block)
