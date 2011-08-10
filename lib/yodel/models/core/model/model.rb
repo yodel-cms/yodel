b6ed08f42284f2460b1d10498d182bbb09f485cc
Will <me@willcannings.com>
2011-08-10 10:09:45 +1000

    Yodel script migration flag

--- lib/yodel/models/core/model/model.rb
+++ lib/yodel/models/core/model/model.rb
@@ -15,6 +15,7 @@ class Model < SiteRecord
   field :triggers, :array
   field :functions, :hash
   field :icon, :string, inherited: true
+  field :menu_root, :boolean, default: false
   field :record_class_name, :string, default: 'Record', inherited: true
   field :searchable, :boolean, default: true, inherited: true
   field :indexes, :array, of: :strings
