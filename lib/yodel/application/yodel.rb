ad463583fb167ab5191df11043662efbf144bde1
Will Cannings <me@willcannings.com>
2011-12-13 11:32:53 +1100

    When loading from gems, only load the latest version of a yodel extension

--- lib/yodel/application/yodel.rb
+++ lib/yodel/application/yodel.rb
@@ -15,8 +15,17 @@ module Yodel
         extension.load!
       end
     else
+      # find the latest version of each yodel extension
+      latest_gem_version = {}
       Gem::Specification.find_all do |gem|
         next unless gem.name.start_with?('yodel_')
+        if !latest_gem_version.key?(gem.name) || gem.version > latest_gem_version[gem.name].version
+          latest_gem_version[gem.name] = gem
+        end
+      end
+      
+      # only load the latest versions
+      latest_gem_version.each_value do |gem|
         extension = GemExtension.new(gem)
         extension.load!
       end
